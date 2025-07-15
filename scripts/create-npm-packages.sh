2d72390d67db9f7c173fd2e15d6e1c2e5fac7098
Abel Penton <abelp@protectgroup.com>
2025-07-15 19:46:31 -0300

    fix distribution package

--- scripts/create-npm-packages.sh
+++ scripts/create-npm-packages.sh
@@ -213,12 +213,12 @@ const { execFileSync } = require('child_process')
 
 function getBinaryPath() {
   const BINARY_DISTRIBUTION_PACKAGES = {
-    'darwin-x64': 'go-blueprint-darwin-x64',
-    'darwin-arm64': 'go-blueprint-darwin-arm64',
-    'linux-x64': 'go-blueprint-linux-x64',
-    'linux-arm64': 'go-blueprint-linux-arm64',
-    //'win32-x64': 'go-blueprint-win32-x64',
-    'win32-arm64': 'go-blueprint-win32-arm64',
+    'darwin-x64': 'go-blueprint-beta-npm-darwin-x64',
+    'darwin-arm64': 'go-blueprint-beta-npm-darwin-arm64',
+    'linux-x64': 'go-blueprint-beta-npm-linux-x64',
+    'linux-arm64': 'go-blueprint-beta-npm-linux-arm64',
+    //'win32-x64': 'go-blueprint-beta-npm-win32-x64',
+    'win32-arm64': 'go-blueprint-beta-npm-win32-arm64',
   }
 
   const binaryName = process.platform === 'win32' ? 'go-blueprint.exe' : 'go-blueprint'
